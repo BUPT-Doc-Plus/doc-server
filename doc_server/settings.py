771f963463976703d2d3db4599567774e836b6b4
liadrinz <liadrinz>
2021-01-10 11:33:43 +0800

    对接mysql

--- doc_server/settings.py
+++ doc_server/settings.py
@@ -110,15 +110,30 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'doc_server.wsgi.application'
+ASGI_APPLICATION = 'doc_server.routing.application'
 
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'doc_plus',
+        'USER': 'root',
+        'PASSWORD': '077925',
+        'HOST': 'www.liadrinz.cn',
+        'PORT': 3306,
+        'TEST': {
+            'CHARSET' : 'utf8',
+            'COLLATION':'utf8_general_ci'
+        }
     }
 }
 
