6efecfb19ba10caf35da6d17851bdaf9ff6d043f
liadrinz <liadrinz>
2021-01-17 01:29:24 +0800

    增加DocTree的管理页面

--- doc/admin.py
+++ doc/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from doc.models import Author, Doc, Access, Token
+from doc.models import Author, Doc, Access, Token, DocTree
 
 # Register your models here.
 admin.site.register(Author)
 admin.site.register(Doc)
 admin.site.register(Access)
 admin.site.register(Token)
+admin.site.register(DocTree)
